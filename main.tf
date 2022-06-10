156ec327cff96ccd96e6e5598896a10a8a30549b
Michael Patsula <michael.patsula@gmail.com>
2022-06-10 16:20:44 -0400

    refactor(dns): remove private DNS zone resource

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_postgresql_flexible_server" "pgsql" {
   resource_group_name = var.resource_group
 
   delegated_subnet_id = var.vnet_create ? azurerm_subnet.pgsql[0].id : data.azurerm_subnet.pgsql[0].id
-  private_dns_zone_id = azurerm_private_dns_zone.private_dns_zone.id
+  private_dns_zone_id = var.private_dns_zone_id
 
   administrator_login    = var.administrator_login
   administrator_password = (var.kv_pointer_enable && length(data.azurerm_key_vault_secret.pointer_sqladmin_password) > 0) ? data.azurerm_key_vault_secret.pointer_sqladmin_password[0].value : var.administrator_password
