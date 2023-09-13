e5ba6d93341e1d1abbc0b8ecef1781af8eee8a4c
Will Hearn <will.hearn@cloud.statcan.ca>
2023-09-13 13:43:25 +0000

    feat(postgresql): Diagnostic settings does not support retention for new diagnostic settings

--- diagnostics.tf
+++ diagnostics.tf
@@ -22,10 +22,6 @@ resource "azurerm_monitor_diagnostic_setting" "postgresql_server" {
     content {
       category = log.value
       enabled  = contains(local.parsed_diag.log, "all") || contains(local.parsed_diag.log, log.value)
-
-      retention_policy {
-        enabled = true
-      }
     }
   }
 
@@ -35,10 +31,6 @@ resource "azurerm_monitor_diagnostic_setting" "postgresql_server" {
     content {
       category = metric.value
       enabled  = contains(local.parsed_diag.metric, "all") || contains(local.parsed_diag.metric, metric.value)
-
-      retention_policy {
-        enabled = true
-      }
     }
   }
 }
